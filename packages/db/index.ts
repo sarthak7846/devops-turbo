aa2989c5b2c38cec347acb139291fef7bc5e5e77
Sarthak <sarthakbehera09@gmail.com>
2026-04-23 22:22:23 +0530

    feat: Initial setup

---
+++ packages/db/index.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+import { dirname, join } from "node:path";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+config({ path: join(__dirname, '.env') })
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
